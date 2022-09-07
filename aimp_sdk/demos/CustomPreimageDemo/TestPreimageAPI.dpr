7f34c1c5561e27b35b4a8154e534b4d8e71525e6
furs <90540827+furstamp@users.noreply.github.com>
2022-09-07 17:20:05 -0300

    Create TestPreimageAPI.dpr

---
+++ aimp_sdk/demos/CustomPreimageDemo/TestPreimageAPI.dpr
@@ -0,0 +1,23 @@
+library TestPreimageAPI;
+
+uses
+  apiPlugin,
+  TestPreimageAPIUnit in 'TestPreimageAPIUnit.pas' {frmTestPreimage};
+
+{$R *.res}
+
+  function AIMPPluginGetHeader(out Header: IAIMPPlugin): HRESULT; stdcall;
+  begin
+    try
+      Header := TTestPreimageAPIPlugin.Create;
+      Result := S_OK;
+    except
+      Result := E_UNEXPECTED;
+    end;
+  end;
+
+exports
+  AIMPPluginGetHeader;
+begin
+
+end.
