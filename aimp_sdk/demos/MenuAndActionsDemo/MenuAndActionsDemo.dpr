f2e541816b9b5ef6d17f4440e0b4cba872d3d2dd
furs <90540827+furstamp@users.noreply.github.com>
2022-09-07 17:35:46 -0300

    Create MenuAndActionsDemo.dpr

---
+++ aimp_sdk/demos/MenuAndActionsDemo/MenuAndActionsDemo.dpr
@@ -0,0 +1,24 @@
+library MenuAndActionsDemo;
+
+uses
+  apiPlugin,
+  MenuAndActionsDemoUnit in 'MenuAndActionsDemoUnit.pas';
+
+{$R *.res}
+
+function AIMPPluginGetHeader(out Header: IAIMPPlugin): HRESULT; stdcall;
+begin
+  try
+    Header := TAIMPMenuAndActionsPlugin.Create;
+    Result := S_OK;
+  except
+    Result := E_UNEXPECTED;
+  end;
+end;
+
+exports
+  AIMPPluginGetHeader;
+
+begin
+
+end.
